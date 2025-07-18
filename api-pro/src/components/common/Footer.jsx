337b7680fd6a9109fe0dc52efc79db7144284085
Malinsha Vithanage <86399536+MalinshaVithanage@users.noreply.github.com>
2025-07-18 18:37:00 +0530

    hero section done

---
+++ api-pro/src/components/common/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+const Footer = () => {
+  return (
+    <div className="footer">
+    <p className="footerText">© 2024 Malinsha Vithanage | All Rights Reserved</p>
+  </div>
+  );
+};
+
+export default Footer;
